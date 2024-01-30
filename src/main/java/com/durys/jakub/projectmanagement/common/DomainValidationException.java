952879d2873a06373e8bd1a58cb5ab35c658e37a
kubavation <37585627+kubavation@users.noreply.github.com>
2024-01-30 19:50:13 +0100

    init project domain

---
+++ src/main/java/com/durys/jakub/projectmanagement/common/DomainValidationException.java
@@ -0,0 +1,8 @@
+package com.durys.jakub.projectmanagement.common;
+
+public class DomainValidationException extends RuntimeException {
+
+    public DomainValidationException(String message) {
+        super(message);
+    }
+}
